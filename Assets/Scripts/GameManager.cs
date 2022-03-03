9004a5e808a1b7dbcf2150a953b34587d0725e40
Karlo <51051819+Karlo121@users.noreply.github.com>
2022-03-03 16:25:52 +0100

    Messin around a bit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject stone;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("sup");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
