df81f39d83ed72453732f3be55050608d6cbb0ea
Sam-Lambda <samer.bishara@mail.mcgill.ca>
2022-03-03 11:20:18 -0500

    spawn stone at (0, 0, -1)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(stone, new Vector3(0, 0, -1), Quaternion.identity);
     }
 
     // Update is called once per frame
